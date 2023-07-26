7bb8048bebe84c4ab11d687cda16bac25bed0dc4
John O'Neill <johnoneill@unity3d.com>
2023-07-26 09:19:11 -0400

    Example using Google Memory Advice API

---
+++ Assets/MemoryAdvice/Runtime/Scripts/MemoryAdvice.cs
@@ -0,0 +1,113 @@
+﻿/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using AOT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MemoryAdvice
+{
+    #region unity_memoryadvice
+    [DllImport("unity_memoryadvice", CharSet = CharSet.Unicode)]
+    private static extern int Unity_MemoryAdvice_init();
+
+    [DllImport("unity_memoryadvice", CharSet = CharSet.Unicode)]
+    private static extern int Unity_MemoryAdvice_initWithParams(String initParams);
+    #endregion
+
+    private delegate void MemoryAdviceWatcherDelegateCallbackForC(int memoryState, IntPtr user_data);
+
+    #region memory_advice
+    [DllImport("memory_advice", CharSet = CharSet.Unicode)]
+    private static extern float MemoryAdvice_getPercentageAvailableMemory();
+
+    [DllImport("memory_advice", CharSet = CharSet.Unicode)]
+    private static extern int MemoryAdvice_getMemoryState();
+
+    [DllImport("memory_advice", CharSet = CharSet.Unicode)]
+    private static extern int MemoryAdvice_registerWatcher(ulong interval,
+        MemoryAdviceWatcherDelegateCallbackForC callback, IntPtr user_data);
+    #endregion
+
+    private static readonly LinkedList<MemoryWatcherDelegateListener> memoryWatcherListeners = new LinkedList<MemoryWatcherDelegateListener>();
+
+    public static MemoryAdviceErrorCode Init()
+    {
+        int CErrorCode = Unity_MemoryAdvice_init();
+        Debug.Log("Init Memory Advice result code: " + CErrorCode);
+        return (MemoryAdviceErrorCode)CErrorCode;
+    }
+
+    public static MemoryState GetMemoryState()
+    {
+        return (MemoryState)MemoryAdvice_getMemoryState();
+    }
+
+    public static MemoryAdviceErrorCode RegisterWatcher(ulong interval, MemoryWatcherDelegateListener callback)
+    {
+        memoryWatcherListeners.AddLast(callback);
+        //call register without any user data
+        return (MemoryAdviceErrorCode)MemoryAdvice_registerWatcher(interval, MemoryWatcherDefaultListener, new IntPtr());
+    }
+
+    public static MemoryAdviceErrorCode UnregisterWatcher(MemoryWatcherDelegateListener callback)
+    {
+        if (memoryWatcherListeners.Remove(callback))
+        {
+            return MemoryAdviceErrorCode.Ok;
+        }
+        else
+        {
+            return MemoryAdviceErrorCode.WatcherNotFound;
+        }
+    }
+
+    [MonoPInvokeCallback(typeof(MemoryAdviceWatcherDelegateCallbackForC))]
+    private static void MemoryWatcherDefaultListener(int memoryState, IntPtr user_data)
+    {
+        Debug.Log("MemoryWatcherCallback state: " + memoryState);
+        for (int i = 0; i < memoryWatcherListeners.Count; i++)
+        {
+            memoryWatcherListeners.ElementAt(i)((MemoryState)memoryState);
+        }
+    }
+}
+
+public enum MemoryState
+{
+    Unknown = 0,  //< The memory state cannot be determined.
+    OK = 1,  //< The application can safely allocate memory.
+    ApproachingLimit = 2,  //< The application should minimize memory allocation.
+    Critical =
+        3,  //< The application should free memory as soon as possible, until
+            //< the memory state changes.
+}
+
+public enum MemoryAdviceErrorCode
+{
+    Ok = 0,  //< No error
+    NotInitialized = -1, //< A call was made before MemoryAdvice was initialized.
+    AlreadyInitialized = -2, //< MemoryAdvice_init was called more than once.
+    LookUpTableInvalid = -3, //< The provided lookup table was not a valid json object.
+    AdvisorParameterInvalid = -4, //< The provided advisor parameters was not a valid json object.
+    WatcherNotFound = -5, //< UnregisterWatcher was called with an invalid callback.
+    TFLiteModelInvalid = -6 //< A correct TFLite model was not provided.
+}
+
+public delegate void MemoryWatcherDelegateListener(MemoryState state);
---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,241 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainManager : MonoBehaviour {
+	public Text textStatus;
+	public Text textVersion;
+	public Text textMemoryAdviceStatus;
+	public Text textLastRunInfo;
+	public Text textAllocationStatus;
+	public Text textAllocatedForGraphics;
+	public Button buttonStartMemTest;
+	public Button buttonPauseMemTest;
+	public InputField inputDelayTime;
+	public InputField inputTextureSize;
+	public InputField inputMemEstimate;
+	public Text headerInputSize;
+	public Toggle checkboxAllocateTextures;
+	public Toggle checkboxAutopauseOnLowMemory;
+
+	private bool allocateTextureMem = true;
+	private float delayAllocationTime = 0.1f;
+	private int textureSize = 512;
+	private ulong delayMemoryAdvice = 500;	// in milliseconds
+
+	private bool paused = false;
+	YieldInstruction waitTimeFrame;
+	private List<Texture2D> listAllocatedTextures = new List<Texture2D>();
+	private List<GameObject> listAllocatedGOs = new List<GameObject>();
+	private StringBuilder sbStatusLog = new StringBuilder();
+	private StringBuilder sbAllocationLog = new StringBuilder();
+	long bytesPerObject = 0;
+
+	void Awake() {
+		//Application.lowMemory += Application_lowMemory;
+		textMemoryAdviceStatus.text = string.Empty;
+		textStatus.text = "Initialized";
+		textAllocationStatus.text = string.Empty;
+		buttonStartMemTest.interactable = true;
+		buttonPauseMemTest.interactable = false;
+		textVersion.text = Application.unityVersion;
+		if (Application.productName.Contains("64bit"))
+			textVersion.text += "\n(64-bit)";
+
+		textVersion.text += "\n(Memory Advice API)";
+
+		textLastRunInfo.text = PlayerPrefs.GetString("lastruninfo");
+		inputMemEstimate.text = "0";
+		textAllocatedForGraphics.text = string.Format("Allocated memory for the graphics driver: {0:N0} bytes", UnityEngine.Profiling.Profiler.GetAllocatedMemoryForGraphicsDriver());
+
+		MemoryAdviceErrorCode errorCode = MemoryAdvice.Init();
+		if (errorCode == MemoryAdviceErrorCode.Ok) {
+			textMemoryAdviceStatus.text = "Memory advice init successfully";
+			RegisterMemAdvice();
+		} else {
+			textMemoryAdviceStatus.text = "Memory advice failed to init";
+		}
+	}
+
+	void RegisterMemAdvice() {
+		MemoryAdviceErrorCode errorCode = MemoryAdvice.RegisterWatcher(delayMemoryAdvice, new MemoryWatcherDelegateListener((MemoryState state) => {
+			switch (state) {
+				case MemoryState.ApproachingLimit:
+					// The application should minimize memory allocation.
+					textMemoryAdviceStatus.text = "<color=yellow>MemoryState.ApproachingLimit</color>";
+					textMemoryAdviceStatus.Rebuild(CanvasUpdate.PreRender);
+					OnPauseMemTest();
+					break;
+				case MemoryState.Critical:
+					// The application should free memory as soon as possible
+					// until the memory state changes.
+					textMemoryAdviceStatus.text = "<color=yellow>MemoryState.Critical</color>";
+					textMemoryAdviceStatus.Rebuild(CanvasUpdate.PreRender);
+					OnPauseMemTest();
+					break;
+			}
+		}));
+
+		if (errorCode == MemoryAdviceErrorCode.Ok) {
+			textMemoryAdviceStatus.text = "Memory advice successfully registered watcher";
+		} else {
+			textMemoryAdviceStatus.text = "Memory advice failed to register watcher";
+		}
+	}
+
+	void OnDestroy() {
+		Application.lowMemory -= Application_lowMemory;
+	}
+
+	private void Application_lowMemory() {
+		textStatus.text = "<color=yellow>Low memory detected</color>";
+
+		if (checkboxAutopauseOnLowMemory.isOn) {
+			OnPauseMemTest();
+			textStatus.text = "<color=yellow>Auto-paused test due to low memory detection</color>";
+			textStatus.Rebuild(CanvasUpdate.PreRender);
+		}
+	}
+
+	public void OnToggleAllocationType() {
+		allocateTextureMem = checkboxAllocateTextures.isOn;
+		headerInputSize.text = allocateTextureMem ? "ARGB32 square texture size:" : "GameObjects:";
+	}
+
+	public void OnStartOrStopMemTest() {
+		if (null == waitTimeFrame)
+			StartMemoryStress();
+		else
+			StopMemoryStress();
+	}
+
+	public void OnPauseMemTest() {
+		// Unpause if paused
+		paused = !paused;
+		textStatus.text = paused ? "Paused" : string.Format("Continuing memory stress... (object size: {0:N0} MB)", bytesPerObject / (1024 * 1024));
+		buttonPauseMemTest.GetComponentInChildren<Text>().text = paused ? "Unpause" : "Pause";
+
+		if (!paused) {
+			// Reset wait timer delay in case it was changed during the pause
+			ReadDelayAllocationTime();
+			waitTimeFrame = new WaitForSeconds(delayAllocationTime);
+		}
+	}
+
+	void ReadDelayAllocationTime() {
+		if (!float.TryParse(inputDelayTime.text, out delayAllocationTime) || delayAllocationTime < 0.0f) {
+			delayAllocationTime = 0.1f;
+			inputDelayTime.text = "0.1f";
+		}
+
+	}
+
+	void ReadAllocationSize() {
+		if (!int.TryParse(inputTextureSize.text, out textureSize) || textureSize <= 0 || textureSize > 8196) {
+			textureSize = 512;
+			inputTextureSize.text = "512";
+		}
+	}
+
+	void StartMemoryStress() {
+		buttonStartMemTest.GetComponentInChildren<Text>().text = "Stop Memory Stress";
+		buttonPauseMemTest.interactable = true;
+		paused = false;
+
+		ReadDelayAllocationTime();
+		ReadAllocationSize();
+
+		if (allocateTextureMem) {
+			var tmpTexture = AllocateTexture();
+			bytesPerObject = UnityEngine.Profiling.Profiler.GetRuntimeMemorySizeLong(tmpTexture);
+			Destroy(tmpTexture);
+		} else {
+			bytesPerObject = UnityEngine.Profiling.Profiler.GetRuntimeMemorySizeLong(new GameObject());
+		}
+		textStatus.text = string.Format("Beginning memory stress... (object size: {0:N0} MB)", bytesPerObject / (1024 * 1024));
+		System.GC.Collect();
+
+		waitTimeFrame = new WaitForSeconds(delayAllocationTime);
+
+		StartCoroutine(AllocateMemoryOverTime());
+	}
+
+	Texture2D AllocateTexture() {
+		return new Texture2D(textureSize, textureSize, TextureFormat.ARGB32, false);
+	}
+
+	void StopMemoryStress() {
+		waitTimeFrame = null;
+		paused = false;
+		textStatus.text = "Ending memory stress";
+		buttonStartMemTest.GetComponentInChildren<Text>().text = "Start Memory Stress";
+		textAllocationStatus.text = string.Format("Flushing {0} allocations and calling GC", allocateTextureMem ? listAllocatedTextures.Count : listAllocatedGOs.Count);
+		buttonPauseMemTest.GetComponentInChildren<Text>().text = "Pause";
+
+		buttonStartMemTest.interactable = true;
+		buttonPauseMemTest.interactable = false;
+		foreach (var texture in listAllocatedTextures) {
+			Destroy(texture);
+		}
+		foreach(var go in listAllocatedGOs) {
+			Destroy(go);
+		}
+		
+		listAllocatedTextures.Clear();
+		listAllocatedGOs.Clear();
+		System.GC.Collect();
+	}
+
+	const int BATCH_ALLOCATE = 100;
+	IEnumerator AllocateMemoryOverTime() {
+		// Use stringbuilder as much as possible to minimize the string creation/disposal per frame
+		// We take 1 hit to pretty-print our number to be command separated (regionally)
+		int batchCount = 0;
+
+		while (null != waitTimeFrame) {
+			if (delayAllocationTime <= 0.0f && textureSize < 1024) {
+				// Allocate in batches of 100 textures the same frame, but only if texture size is < 1024
+				if (--batchCount < 0)
+					batchCount = BATCH_ALLOCATE;
+				else
+					yield return null;
+			} else {
+				yield return waitTimeFrame;
+			}
+			
+			if (paused)
+				continue;
+
+			if (null == waitTimeFrame)
+				break;
+
+			sbStatusLog.Clear().Append("Last run generated: ");
+
+			sbAllocationLog.Clear().Append("Current allocations: ");
+
+			if (allocateTextureMem) {
+				listAllocatedTextures.Add(AllocateTexture());
+				sbAllocationLog.Append(listAllocatedTextures.Count).Append(" textures");
+				sbStatusLog.Append(listAllocatedTextures.Count).Append(" textures at size ").Append(textureSize);
+			} else {
+				for (int i = 0; i < textureSize; i++)
+					listAllocatedGOs.Add(new GameObject());
+
+				sbAllocationLog.Append(listAllocatedGOs.Count).Append(" GameObjects");
+				sbStatusLog.Append(listAllocatedGOs.Count).Append(" GameObjects");
+			}
+			long total = (bytesPerObject * listAllocatedTextures.Count) / (1024 * 1024);
+			string totalStr = string.Format("{0:N0}", total);
+			inputMemEstimate.text = totalStr;
+
+			sbStatusLog.Append(" (total allocated = ").Append(totalStr).Append(" MB)");
+			textAllocationStatus.text = sbAllocationLog.ToString();
+			PlayerPrefs.SetString("lastruninfo", sbStatusLog.ToString());
+			PlayerPrefs.Save();
+
+
+		}
+	}
+}
